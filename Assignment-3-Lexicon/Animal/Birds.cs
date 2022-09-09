8df218322d7ef83cea5fe1af5e8480267fd145ab
muhammadnaveedahmed <mnahmed786@gmail.com>
2022-09-09 11:25:12 +0200

    Birds

---
+++ Assignment-3-Lexicon/Animal/Birds.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_Lexicon.Animal
+{
+	class Birds : Animal
+	{
+		private string? aniname;
+		private int age;
+		private double weight;
+		private int nroflegs;
+
+        public Birds(string aniname, double weight, int age, int nroflegs, double wingSpan)
+        {
+            WingSpan = wingSpan;
+        }
+
+        public double WingSpan		
+		{
+			get;
+			 
+		}
+
+		
+		public override string DoSound()
+		{
+			return "choon choon";
+		}
+
+		public override string Stats()
+		{
+			return base.Stats() + $"\tWing span: {WingSpan} m";
+		}
+	}
+}
